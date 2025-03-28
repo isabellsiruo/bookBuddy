d46ca14418365cf55ccbf45f817f3f4d1c52a96c
Isabell Ventouris <isv2107@columbia.edu>
2025-03-28 12:34:39 -0400

    created Navbar component with basic navigation

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,1 +1,13 @@
-/* TODO - add your code to create a functional React component that renders a navigation bar for the different views in your single page application. You may consider conditionally rendering some options - for example 'Login' should be available if someone has not logged in yet. */
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <nav style={{ padding: '10px', backgroundColor: '#eee' }}>
+      <Link to="/books" style={{ marginRight: '10px' }}>All Books</Link>
+      <Link to="/login" style={{ marginRight: '10px' }}>Login</Link>
+      <Link to="/register" style={{ marginRight: '10px' }}>Register</Link>
+      <Link to="/account">Account</Link>
+    </nav>
+  );
+}
+
