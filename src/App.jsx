cb13d7b029303fda535d1e10b4f17548aed2c47e
Isabell Ventouris <isv2107@columbia.edu>
2025-03-28 10:39:52 -0400

    added React Router with all required routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Books from './pages/Books';
+import SingleBook from './pages/SingleBook';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Account from './pages/Account';
+
+export default function App() {
   return (
-    <div>
-      <h1>Welcome to BookBuddy</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/books" element={<Books />} />
+        <Route path="/singlebook/:id" element={<SingleBook />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
-
